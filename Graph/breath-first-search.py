fedb1ed1a8faa7ab9655f40a7ebcd6b71f68e015
SchadenKai <kairustecsonpersonal@gmail.com>
2023-08-29 17:12:01 +0800

    modified breadth-first-search

--- Graph/breath-first-search.py
+++ Graph/breath-first-search.py
@@ -1,29 +1,40 @@
 from collections import deque
 
 def breadth_first(graph, src, dest): 
+    # search queue = frontier 
+    # this is the queue of nodes to be searched
     search_queue = deque()
+    # this list represents the searched nodes 
     searched = []
     # use += operator for inserting elements, not the whole list
+    # start with the initial state / source node
     search_queue += (graph[src])
+    
     count = 0
+    # while the queue is not empty, continue searching / looping
     while search_queue:
-        print(count)
+        
+        # count the number of steps taken to find the target
         count+=1
+        
         curr = search_queue.popleft()
+        # if the current node is not in the searched list, add it to the queue / frontier 
         if curr not in searched:
             if curr == dest: 
-                return("Found the target: " + curr)
+                return("Found the target: " + curr + " in " + str(count) + " steps")
             else: 
                 search_queue += (graph[curr])
                 searched.append(curr)
     return("Not found: " + dest)
         
-dict = {}
-dict["alice"] = ["bob", "kier", "tani"]
-dict["bob"] = ["kairus"]
-dict["kier"] = ["kairus"]
-dict["tani"] = ["jhasmine"]
-dict["kairus"] = []
-dict["jhasmine"] = []
+relationships = {
+    "Alice": ["Bob", "Charlie"],
+    "Bob": ["David", "Eve"],
+    "Charlie": ["David"],
+    "David": ["Eve"],
+    "Eve": [],
+    "Frank": ["Grace"],
+    "Grace": []
+}
 
-print(breadth_first(dict, "alice", "kairus"))
+print(breadth_first(relationships, "Alice", "David"))
